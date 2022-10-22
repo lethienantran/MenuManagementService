7e3e7ca41df2614043ec4f50c9fd76ccdbe6b3a3
spulethienantran <trana20@spu.edu>
2022-10-22 12:23:24 -0700

    Label Added in Master

--- SelfOrder.Master.designer.cs
+++ SelfOrder.Master.designer.cs
@@ -24,6 +24,15 @@ namespace SelfOrderManagementSystem
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
+        /// brandNameLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label brandNameLabel;
+
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
