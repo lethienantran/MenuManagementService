65de24b12d2ac7be916abb75513d6b6705039d97
spulethienantran <trana20@spu.edu>
2022-10-22 20:05:19 -0700

    as

--- Home.aspx
+++ Home.aspx
@@ -1,5 +1,42 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SelfOrder.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SelfOrderManagementSystem.Home" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="StyleSheet.css" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div align="center">
+        <h3>Add/Change Items</h3>
+        <table style="border: 1px solid black;">
+            <tr >
+                <th>
+                    Category
+                </th>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Description
+                </th>
+                <th>
+                    Price
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="categoryText" placeholder="Enter Category" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:TextBox ID="nameText" placeholder="Enter Name" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:TextBox ID="descriptionText" placeholder="Enter Description" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:TextBox ID="priceText" placeholder="Enter Price" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+        <asp:Button ID="addButton" runat="server" Text="ADD" CssClass="roundedButton" Font-Size="15px" ForeColor="White" OnClick="addButton_Click"/>
+                    &nbsp;&nbsp;
+        <asp:Button ID="deleteButton" runat="server" Text="DELETE" CssClass="roundedButton" Font-Size="15px" ForeColor="White" OnClick="deleteButton_Click"/>
+    </div>
 </asp:Content>
--- Home.aspx.cs
+++ Home.aspx.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +13,59 @@ namespace SelfOrderManagementSystem
 {
     public partial class Home : System.Web.UI.Page
     {
+        string CS = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        int UserID;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null)
+            {
+                Response.Redirect("StartHome.aspx");
+            }
+            else
+            {
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlCommand command = new SqlCommand("AddCategory", con);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@userID", Session["UserID"].ToString());
+                    command.Parameters.AddWithValue("@categoryname", categoryText.Text);
+                    command.Parameters.AddWithValue("@itemname", nameText.Text);
+                    command.Parameters.AddWithValue("@itemdescription", descriptionText.Text);
+                    command.Parameters.AddWithValue("@itemprice", priceText.Text);
+
+                    con.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        protected void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null)
+            {
+                Response.Redirect("StartHome.aspx");
+            }
+            else
+            {
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlCommand command = new SqlCommand("DeleteCategory", con);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@userID", Session["UserID"].ToString());
+                    command.Parameters.AddWithValue("@categoryname", categoryText.Text);
+                    command.Parameters.AddWithValue("@itemname", nameText.Text);
+                    command.Parameters.AddWithValue("@itemdescription", descriptionText.Text);
+                    command.Parameters.AddWithValue("@itemprice", priceText.Text);
+
+                    con.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
